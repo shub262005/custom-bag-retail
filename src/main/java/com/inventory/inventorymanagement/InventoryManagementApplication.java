483ce922c6c74f4123ec091c4d39938245951ac6
shubham <shubhamjain26tt@gmail.com>
2026-08-26 18:48:45 +0530

    chore: configure PostgreSQL database

--- src/main/java/com/inventory/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/inventory/inventorymanagement/InventoryManagementApplication.java
@@ -2,13 +2,8 @@ package com.inventory.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class InventoryManagementApplication {
 
     public static void main(String[] args) {
